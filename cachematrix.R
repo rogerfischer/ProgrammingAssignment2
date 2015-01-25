3e9b98b6e02a70c0aa50a99b376f36c395a4b59c
Roger Fischer <roger.fischer@kaywa.com>
2015-01-25 22:45:25 +0100

    Commented and finished functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,54 @@
 ## Cache the inverse of a matrix.
+##
+## Usage
+## =====
+## source("cachematrix.R")
+## x <- stats::rnorm(16)
+## dim(x) <- c(4,4)
+## mx <- makeCacheMatrix(x)
+## cacheSolve(mx)
+## cacheSolve(mx)  # The second time you will get the already cached data
+
 
 ## makeCacheMatrix: This function creates a special "matrix" object that 
-## can cache its inverse.
+## can cache its inverse. 
+## Important: Matrix must be square!
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    ## print(environment())
+    ## evn <- environment()
+    ## print(parent.env(evn))
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(solve) m <<- solve
+    
+    getmatrix <- function() m
+    getevn <- function() environment()
+    list(set = set, get = get,
+        setmatrix = setmatrix,
+        getmatrix = getmatrix)
+        ## ,getevn = getevn)
 }
 
 
-
-## cacheSolve: This function computes the inverse of the special "matrix" 
+## cacheSolve: This function computes the inverse of the special "matrix" 
 ## returned by makeCacheMatrix above. If the inverse has already been 
 ## calculated (and the matrix has not changed), then the cachesolve should 
-## retrieve the inverse from the cache.
+## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getmatrix()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setmatrix(m)
-        m
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+            message("Getting cached data.")
+            return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
